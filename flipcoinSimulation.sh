be177d75ac3484e5362d6f95e60f9e997a2d4c89
deku418 <mayurvarade397@gmail.com>
2021-03-31 10:15:04 +0530

    Add_countHeadTail

--- flipcoinSimulation.sh
+++ flipcoinSimulation.sh
@@ -4,6 +4,8 @@ echo "Welcome to FlipCoin Simulation Program"
 
 Head=1
 Tail=0
+heads=0
+tails=0
 Toss=$((RANDOM%2))
 if [ $Toss -eq $Head ]
 then
@@ -11,3 +13,16 @@ then
 else
 	echo "Tail Win"
 fi
+
+while [ $valid ]
+Toss=$((RANDOM%2))
+do
+	if [ $Toss -eq $Head ]
+	then
+		((heads++))
+	else
+		((tails++))
+	fi
+done
+
+echo "Head Win : $heads times"  "Tail Win : $tails times"
